db19829a3f4555150122364d72501838b7b8144d
Soren Rood <sorenrood@gmail.com>
2021-01-27 21:26:35 -0800

    Added sample code to purchase shares of apple.

--- robin.py
+++ robin.py
@@ -1,5 +1,6 @@
 import robin_stocks as r
 import os
-login = r.login(os.getenv('SOREN_EMAIL'), os.getenv('SOREN_ROBINHOOD_PASSWORD'))
-pos = r.get_all_watchlists()
-print(pos)
+
+r.login(os.getenv('SOREN_EMAIL'), os.getenv('SOREN_ROBINHOOD_PASSWORD'))
+x = r.order_buy_limit("AAPL", 1, 300)
+print(x)
