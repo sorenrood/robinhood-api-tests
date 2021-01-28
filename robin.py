279572f7be1dfed6e6a10acec4f35f220dee9170
Soren Rood <sorenrood@gmail.com>
2021-01-27 21:30:08 -0800

    Added thingy to get apple price..

--- robin.py
+++ robin.py
@@ -2,5 +2,6 @@ import robin_stocks as r
 import os
 
 r.login(os.getenv('SOREN_EMAIL'), os.getenv('SOREN_ROBINHOOD_PASSWORD'))
-x = r.order_buy_limit("AAPL", 1, 300)
-print(x)
+price = r.stocks.get_latest_price('AAPL', includeExtendedHours=True)
+price = float(price[0])
+print(f"The current price of AAPL is {price}")
